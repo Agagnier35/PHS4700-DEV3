529e0b8bb727492471956316e2d11d6b3efa1963
Agagnier35 <felix_agagnier@hotmail.ca>
2017-11-09 02:12:42 -0500

    Delete verifierCollionIncertaine.m

--- verifierCollionIncertaine.m
+++
@@ -1,3 +0,0 @@
-function bool = verifierCollionIncertaine(voitureA, voitureB)
-bool = norm(voitureA.position - voitureB.position) == (CalculerRayonSphereAuto(voitureA) + CalculerRayonSphereAuto(voitureB));
-    
